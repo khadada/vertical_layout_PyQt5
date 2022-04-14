aed879955c984e1ddd89387b691da9d2be67881d
khadada <lkhadada@gmail.com>
2022-04-14 08:07:51 +0100

    I create cutom class of QWidget like placeholder widget for easy visualize the layout

--- custom_widget.py
+++ custom_widget.py
@@ -1,3 +1,15 @@
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QColor, QPalette
 
+class Color(QWidget):
+    def __init__(self,color):
+        super().__init__()
+        # turn on the autoFill 
+        self.setAutoFillBackground(True)
+        # Create palette which is wrap (غلاف)
+        palette = self.palette()
+        # Set the color of the wrap (غلاف) to color parameter 
+        palette.setColor(QPalette.Window,QColor(color))
+        # set the this class to wear the wrap (غلاف) 
+        self.setPalette(palette)
+        
